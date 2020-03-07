c0f73a58cbe0b4d78b3ee3a77798bd83d25a0fca
Hazmi35 <contact@hzmi.xyz>
2020-03-07 23:58:25 +0700

    hmm

--- src/Main.ts
+++
@@ -1,7 +0,0 @@
-import Client from "./handlers/Rendang";
-import EventHandler from "./handlers/Events";
-
-const client = new Client({ disableMentions: "everyone", fetchAllMembers: true });
-
-client.login(process.env.DISCORD_TOKEN);
-EventHandler(client);
