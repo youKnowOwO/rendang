c724c07d9055e53a0192e2a1a54f91794fc489ee
Hazmi35 <contact@hzmi.xyz>
2020-03-08 00:03:08 +0700

    Update Ready.ts

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,7 +1,7 @@
 import BotClient from "../handlers/BotClient";
 
 const code = (client: BotClient): void => {
-    console.log(`${client.user?.username} is ready to serve!`);
+    console.log(`${client.user?.username} is ready to serve ${client.users.cache.size} users on ${client.guilds.cache.size} guilds in ${client.guilds.cache.size} channels!`);
 };
 
 const name = "ready";
