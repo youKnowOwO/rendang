dba37ddb8508473e3130602b0a205fdf37a3b267
HZMI - Hazmi35 <contact@hzmi.xyz>
2020-03-12 23:42:28 +0700

    Update EvalCommand.ts

--- src/commands/developer/EvalCommand.ts
+++ src/commands/developer/EvalCommand.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-eval */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import BaseCommand from "../../handlers/structures/BaseCommand";
 import BotClient from "../../handlers/BotClient";
 import Message from "../../typings/Message";
