6bbcb6ba91b0db8ddafe52148bbe6d0a74e26c17
Hazmi35 <contact@hzmi.xyz>
2020-03-20 16:41:06 +0700

    It's done.

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -6,7 +6,9 @@ export default class ReadyEvent implements EventProp {
     constructor(private client: BotClient) {}
 
     public run(): void {
-        console.log(`${this.client.user!.username} is ready to serve ${this.client.users.cache.size} users on ${this.client.guilds.cache.size} guilds in ${this.client.channels.cache.size} channels!`);
+        console.log(`${this.client.user!.username} is ready to serve ${this.client.users.cache.size - 1} users on ${this.client.guilds.cache.size} guilds in `
+            + `${this.client.channels.cache.filter(ch => ch.type === "text").size} text channels `
+            + `and ${this.client.channels.cache.filter(ch => ch.type === "voice").size} voice channels!`);
         return undefined;
     }
 }
