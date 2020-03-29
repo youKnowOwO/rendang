3fef4c2bc0d3bcba0033e16a7834c1031388fd15
Hazmi35 <contact@hzmi.xyz>
2020-03-29 19:56:07 +0700

    Some changes

--- src/database/Controller.ts
+++ src/database/Controller.ts
@@ -1,88 +1,87 @@
-// import mongoose from "mongoose";
-// import model from "./models/Guild.model";
+import mongoose from "mongoose";
 
-// export default class DatabaseController {
-//     constructor(private BaseModel: CrateSchema) {}
-//     public get(id, validate = false) {
-//         return new Promise((resolve, reject) => {
-//             if (validate) {
-//                 if (!mongoose.Types.ObjectId.isValid(id)) reject(new Error("Please provide correct ID"));
-//             }
-//             this.BaseModel.findById(id).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+export default class DatabaseController {
+    constructor(private BaseModel: mongoose.Schema) {}
+    public get(id, validate = false): Promise<object> {
+        return new Promise((resolve, reject) => {
+            if (validate) {
+                if (!mongoose.Types.ObjectId.isValid(id)) reject(new Error("Please provide correct ID"));
+            }
+            this.BaseModel.findById(id).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public getBulk(filter = {}) {
-//         return new Promise((resolve, reject) => {
-//             this.BaseModel.find(filter).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+    public getBulk(filter = {}) {
+        return new Promise((resolve, reject) => {
+            this.BaseModel.find(filter).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public getOne(filter = {}) {
-//         return new Promise((resolve, reject) => {
-//             this.BaseModel.findOne(filter).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+    public getOne(filter = {}) {
+        return new Promise((resolve, reject) => {
+            this.BaseModel.findOne(filter).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public create(data) {
-//         let newdata = new this.BaseModel(data);
-//         return new Promise((resolve, reject) => {
-//             newdata.save().then(data => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+    public create(data) {
+        let newdata = new this.BaseModel(data);
+        return new Promise((resolve, reject) => {
+            newdata.save().then(data => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public delete(id, validate = false) {
-//         return new Promise((resolve, reject) => {
-//             if (validate) {
-//                 if (!mongoose.Types.ObjectId.isValid(id)) reject(new Error("Please provide correct ID"));
-//             }
-//             this.BaseModel.findOneAndRemove({
-//                 _id: id
-//             }).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+    public delete(id, validate = false) {
+        return new Promise((resolve, reject) => {
+            if (validate) {
+                if (!mongoose.Types.ObjectId.isValid(id)) reject(new Error("Please provide correct ID"));
+            }
+            this.BaseModel.findOneAndRemove({
+                _id: id
+            }).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public deleteBulk(filter = {}) {
-//         return new Promise((resolve, reject) => {
-//             this.BaseModel.remove(filter).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
+    public deleteBulk(filter = {}) {
+        return new Promise((resolve, reject) => {
+            this.BaseModel.remove(filter).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 
-//     public set(id, data = {}, options = { multi: false }) {
-//         return new Promise((resolve, reject) => {
-//             this.BaseModel.update({
-//                 _id: id
-//             }, {
-//                 $set: data
-//             }, options).then((data) => {
-//                 resolve(data);
-//             }).catch((err) => {
-//                 reject(err);
-//             });
-//         });
-//     }
-// }
+    public set(id, data = {}, options = { multi: false }) {
+        return new Promise((resolve, reject) => {
+            this.BaseModel.update({
+                _id: id
+            }, {
+                $set: data
+            }, options).then((data) => {
+                resolve(data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+}
