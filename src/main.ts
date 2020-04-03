0b8b99c047eb39a13369f2770d7448e5dd33d835
Hazmi35 <contact@hzmi.xyz>
2020-04-03 22:01:34 +0700

    Small changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { Promise as Bluebird } from "bluebird";
 import { useDotEnv, useBlueBird } from "./config.json";
-import { config as LoadDotEnv } from "dotenv";
+import { config as loadDotEnv } from "dotenv";
 import Client from "./handlers/BotClient";
 
 if (useBlueBird) global.Promise = Bluebird;
-if (useDotEnv) LoadDotEnv({ path: "./src/.env" });
+if (useDotEnv) loadDotEnv({ path: "./src/.env" });
 
-const client = new Client({ disableMentions: "everyone", fetchAllMembers: true })
-    .setToken(process.env.DISCORD_TOKEN as string);
-
-client.build();
+new Client({ disableMentions: "everyone", fetchAllMembers: true })
+    .setToken(process.env.DISCORD_TOKEN as string)
+    .build();
