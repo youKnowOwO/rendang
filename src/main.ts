be7c2e9c0d5ee1e50a805e88aed35cd6bea8cb77
Hazmi35 <contact@hzmi.xyz>
2020-04-03 22:04:26 +0700

    Move .env to the root dir again..

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { config as loadDotEnv } from "dotenv";
 import Client from "./handlers/BotClient";
 
 if (useBlueBird) global.Promise = Bluebird;
-if (useDotEnv) loadDotEnv({ path: "./src/.env" });
+if (useDotEnv) loadDotEnv();
 
 new Client({ disableMentions: "everyone", fetchAllMembers: true })
     .setToken(process.env.DISCORD_TOKEN as string)
