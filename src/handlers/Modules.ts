4087360df811cc7de7190ac0d7dddf6b51585fb7
Hazmi35 <contact@hzmi.xyz>
2020-04-05 17:42:10 +0700

    Use .filter to filter files in commands folder

--- src/handlers/Modules.ts
+++ src/handlers/Modules.ts
@@ -6,18 +6,11 @@ export default class ModulesLoader {
     constructor(private client: BotClient, readonly path: string) {}
 
     public build(): BotClient {
-        readdir(this.path, (err, categories: string[]) => {
+        readdir(this.path, (err, categoriesRaw: string[]) => {
             if (err) {
                 this.client.log.error("MODULES_LOADER_ERR: ", err);
             }
-            categories.forEach(category => {
-                if (category.endsWith(".schema")) {
-                    const index = categories.indexOf(category);
-                    if (index > -1) {
-                        categories.splice(index, 1);
-                    }
-                }
-            });
+            const categories = categoriesRaw.filter(c => !c.endsWith(".schema"));
             this.client.log.info(`Found ${categories.length} categories.`);
             categories.forEach(category => {
                 const moduleConf: ModuleConf = require(`${this.path}/${category}/module.json`);
@@ -25,13 +18,12 @@ export default class ModulesLoader {
                 moduleConf.cmds = [];
                 if (!moduleConf) return undefined;
                 this.client.commandsHandler.helpMeta.set(category, moduleConf);
-                const regex = /(?!json)(ts|js)/gi;
-                readdir(`${this.path}/${category}`, (err, files: string[]) => {
-                    this.client.log.info(`Found ${files.filter(file => regex.exec(file) !== null).length} command(s) from ${category}`);
+                readdir(`${this.path}/${category}`, (err, filesRaw: string[]) => {
+                    const files = filesRaw.filter(f => f.endsWith("js") || f.endsWith(".ts"));
                     if (err) this.client.log.error("MODULES_LOADER_ERR: ", err);
+                    this.client.log.info(`Found ${files.length} command(s) from ${category}`);
                     const disabledCommands: string[] = [];
                     files.forEach(file => {
-                        if (!file.endsWith(".js") && !file.endsWith(".ts")) return undefined;
                         const prop: CommandComponent = new (require(`${this.path}/${category}/${file}`).default)(this.client, { category, path: `${this.path}/${category}/${file}`});
                         if (prop.conf.disable) disabledCommands.push(prop.help.name);
                         this.client.commandsHandler.commands.set(prop.help.name, prop);
